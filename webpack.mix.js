e732fd60932e3c82c6c42275da9691ec2829df5c
Sean O'Donnell <contact@seanodonnell.co.uk>
2025-08-26 11:35:05 +0100

    FNAV-2 Adjustments to the NPM build process

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 const mix = require('laravel-mix')
 
 mix
-    .postCss('resources/css/plugin.css', 'resources/dist/plugin.css', [
-        require('tailwindcss')
-    ])
+    .postCss('resources/css/plugin.css', 'resources/dist/plugin.css')
     .js('resources/js/plugin.js', 'resources/dist/plugin.js');
